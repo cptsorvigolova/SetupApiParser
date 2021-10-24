b550931a10473ba0144c9b8bbe68b5e0304f74a5
klimburov <klimburov@mail.ru>
2021-10-24 20:25:34 +0400

    Update Program.cs

--- SetupApiParser/SetupApiParserTest/Program.cs
+++ SetupApiParser/SetupApiParserTest/Program.cs
@@ -11,10 +11,12 @@ namespace SetupApiParserTest
         {
             var setupApiPath = @"D:\source\repos\usb_forensic\Sources&IoCs\setupapi\setupapi.dev.log";
             setupApiPath = @"D:\source\repos\usb_forensic\Sources&IoCs\setupapi\setupapi.dev.20210918_134609.log";
+            setupApiPath = @"C:\Windows\INF\setupapi.dev.log";
+            var files = Directory.GetFiles(@"C:\Windows\INF", @"setupapi.dev.*.log");
             var setupApi = File.ReadAllText(setupApiPath);
             var parser = new SetupApiParser.SetupApiParser();
             var result = parser.Parse(setupApi);
-            var sections = result.BootSessions.SelectMany(x => x.Sections).OrderBy(x => x.ToString()).ToList();
+            var sections = result.BootSessions.SelectMany(x => x.Sections).Where(x=>x.Header.Contains("[Device Install")).OrderBy(x => x.ToString()).ToList();
 
             Console.ReadLine();
         }
